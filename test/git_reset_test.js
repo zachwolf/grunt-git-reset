7b6e033434a48649c35efb9cdceb0d738056873e
Zach Wolf <hello@zachwolf.com>
2014-02-03 21:54:20 -0600

    Adds tests

--- test/git_reset_test.js
+++ test/git_reset_test.js
@@ -1,6 +1,11 @@
 'use strict';
 
-var grunt = require('grunt');
+var grunt = require('grunt'),
+    path  = require('path'),
+    exec  = require('child_process').exec,
+    execOptions = {
+      cwd: path.join(__dirname, '..')
+    };
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -24,25 +29,43 @@ var grunt = require('grunt');
 
 exports.git_reset = {
   setUp: function(done) {
-    // setup here if necessary
     done();
   },
-  default_options: function(test) {
+  bad_options: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    test.throws(
+      exec('grunt git_reset:bad_options', execOptions),
+      Error,
+      'Running git_reset without path setting throws an error'
+    );
 
     test.done();
   },
-  custom_options: function(test) {
-    test.expect(1);
+  good_options: function(test) {
+    var file_exists,
+        file_contents;
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+    test.expect(4);
 
-    test.done();
+    // delete file
+    grunt.file.delete('test/fixtures/delete_this_file');
+    file_exists = grunt.file.exists('test/fixtures/delete_this_file');
+    test.ok(!file_exists, 'file is deleted to start out');
+
+    // edit file
+    grunt.file.write('test/fixtures/edit_this_file', 'changed');
+    file_contents = grunt.file.read('test/fixtures/edit_this_file');
+    test.equal(file_contents, 'changed', 'file is changed');
+
+    exec('grunt git_reset:good_options', execOptions, function(error, stdout) {
+      file_exists = grunt.file.exists('test/fixtures/delete_this_file');
+      test.ok(file_exists, 'deleted file restored');
+
+      file_contents = grunt.file.read('test/fixtures/edit_this_file');
+      test.equal(file_contents, 'unchanged', 'file is unchanged');
+
+      test.done();
+    });
   }
 };
